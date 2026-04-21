098390aefdcec108152e2f58defdefcc2072b2fc
MONGU38 <fbi1588@gmail.com>
2026-04-21 12:19:44 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'woodan-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/woodan/'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
