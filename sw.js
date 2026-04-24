8712a35ed9b3074b29f321a3cdba8e886b286b83
MONGU38 <fbi1588@gmail.com>
2026-04-24 09:52:55 +0900

    Prevent fetch event for sw.js file

--- sw.js
+++ sw.js
@@ -7,5 +7,6 @@ self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
 });
 self.addEventListener('fetch', e => {
+  if (e.request.url.includes('sw.js')) return;
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
