a667220f3233e2e713907b9d855c4ab792cba743
MONGU38 <fbi1588@gmail.com>
2026-04-24 09:47:43 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-const CACHE = 'woodan-v1';
+const CACHE = 'woodan-' + new Date().toISOString();
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/woodan/'])));
 });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
